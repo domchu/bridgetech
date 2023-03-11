ebe31a8b13eb7a17b455bc5bd7584d7a7d45c5d8
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-11 23:02:22 +0100

    done with chakra setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
 
+import React from "react"
+import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react'
+import "../styles/globals.css";
+import { ChakraBaseProvider  } from '@chakra-ui/react'
+
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      1: "#092038",
+      2: "#1a202c",
+    },
+    secondary: {
+      1: "red",
+      2: "#1a202c",
+    },
+    accent: {
+      1: "#f7fafc",
+      2: "#1a202c",
+    },
+    other: {
+      1: "#f7fafc",
+      2: "#1a202c",
+    },
+  },
+});
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraBaseProvider >
+     <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+      </ChakraProvider>
+      </ChakraBaseProvider >
+  )
 }
 
 export default MyApp
