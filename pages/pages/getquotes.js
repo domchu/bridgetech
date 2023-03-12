22988cb96403588cc640797e577363c66e6f40cf
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-12 23:57:32 +0100

    chores:created the quotes files

---
+++ pages/pages/getquotes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from "next/head"
+
+const getquotes = () => {
+  return (
+    <>
+       <Head>
+        <title>BridgeTech | Get Quotes</title>
+        <meta
+          name="courses"
+          content="you can select from our wide range of courses"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </>
+  )
+}
+
+export default getquotes
