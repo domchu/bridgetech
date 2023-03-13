08270984980ac75c04d85df2b7fffdb63c2369a4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-13 23:11:50 +0100

    structure:created the files structure

---
+++ Components/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>
+      <h2>Blog page</h2>
+    </div>
+  )
+}
+
+export default Blog
+
---
+++ Components/Faq/Faq.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Faq = () => {
+  return (
+    <div>
+      <h1>Faq page</h1>
+    </div>
+  )
+}
+
+export default Faq
