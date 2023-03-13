410641b05b857482c13cfce7e84d2651b530f8be
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-13 23:15:43 +0100

    created more files structure

---
+++ Components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>my projects</h1>
+    </div>
+  )
+}
+
+export default Projects
---
+++ Components/Testimonails/Testimonials.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h1>testionals slider</h1>
+    </div>
+  )
+}
+
+export default Testimonials
