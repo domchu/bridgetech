d9e1813b029dc380cbbb07b164a043a6465b0662
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-24 21:39:00 +0100

    chores:added the navbar text

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "../Navbar/Navdata";
+import outline from "../../public/images/outline.png";
 
-const Header = () => {
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
-    <div>
-      <h1>the header</h1>
-    </div>
-  )
+    <>
+      <nav>
+        <Box className="nav-center">
+          {/* the logo section */}
+          <Box className="nav-header">
+            <Image src={outline} className="brand-logo" alt="brand Logo" />
+            <button
+              className="nav-toggle"
+              onClick={() => setShowLinks(!showLinks)}
+            >
+              <FaBars />
+            </button>
+          </Box>
+          {/* the navbar section */}
+          <div className="links-container" ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <Link href={url} passhref="true">
+                      {text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {/* the social media section */}
+          <Box>
+            <ul className="social-icons">
+              {social.map((socialIcon) => {
+                const { id, url, icon } = socialIcon;
+                return (
+                  <li key={id}>
+                    <Link href={url} passhref="true">
+                      {icon}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </Box>
+        </Box>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <Link
+href='/about'
+className={currentRoute === '/about' ? styles.active : styles.nonActive}
+>
+About
+</Link> */
 }
 
-export default Header
+/* Specific styles for non-active links */
+// .nonActive {
+//   color: white;
+// }
+
+/* Specific styles for active links */
+// .active {
+//   color: black;
+//   background: orange;
+// }
