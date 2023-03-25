73c7f110fa686fbb7350b8c24fa2372ebd235f74
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-25 14:21:47 +0100

    imported the topnav & header to  login & sign up

--- pages/access/signin.js
+++ pages/access/signin.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Head from "next/head"
+import Header from "../../Components/Header/Header";
+import Topnav from "../../Components/Topnav/Topnav";
 
 const signin = () => {
   return (
     <>
-       <Head>
-        <title>BridgeTech | Sign In</title>
+      <Head>
+        <title>BridgeTech | Sign up</title>
         <meta
           name="courses"
           content="you can select from our wide range of courses"
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <Topnav />
+      <Header />
+      <h1>Sign up page</h1>
     </>
-  )
-}
+  );
+};
 
 export default signin
