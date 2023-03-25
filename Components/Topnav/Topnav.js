42b934a6550c583551bbb715b139d2c7ce4691fd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-25 13:15:59 +0100

    chores:clients texts views

--- Components/Topnav/Topnav.js
+++ Components/Topnav/Topnav.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
+import { Link } from "next/link";
 
 const Topnav = () => {
   return (
     <>
       <Flex
-        justify="space-betweem"
+        justifyContent="space-betweem"
         margin="auto"
         width="80%"
         align="center"
         textAlign="center"
         flexDirection={{ base: "column", sm: "column", md: "row", lg: "row" }}
       >
-        <Box></Box>
-        <Box></Box>
+        <Box>
+          <ul>
+            <li>
+              <Link href="/" passhref="true">
+                info@example.com
+              </Link>
+            </li>
+            <li>
+              <Link href="Tel:+234 07034278995" passhref="true">
+                +234 (0) 70 3427 8995
+              </Link>
+            </li>
+          </ul>
+        </Box>
+        <Box>
+          <ul>
+            <li>
+              <Link href="/access/login" passhref="true">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link href="/access/signin" passhref="true">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </Box>
       </Flex>
       <h1>Topnav</h1>
     </>
