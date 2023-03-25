64fe4285d7f75f649f8ef15c29f52f5b09986925
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-25 13:27:19 +0100

    chores:display the topnav items

--- Components/Topnav/Topnav.js
+++ Components/Topnav/Topnav.js
@@ -1,48 +1,56 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
-import { Link } from "next/link";
+import Link from "next/link";
 
 const Topnav = () => {
   return (
     <>
       <Flex
-        justifyContent="space-betweem"
+        className="topnav-container"
+        justifyContent="space-around"
         margin="auto"
-        width="80%"
+        width="90%"
         align="center"
         textAlign="center"
         flexDirection={{ base: "column", sm: "column", md: "row", lg: "row" }}
       >
         <Box>
-          <ul>
-            <li>
-              <Link href="/" passhref="true">
-                info@example.com
-              </Link>
-            </li>
-            <li>
-              <Link href="Tel:+234 07034278995" passhref="true">
-                +234 (0) 70 3427 8995
-              </Link>
-            </li>
-          </ul>
+          <Flex>
+            <Box>
+              <li>
+                <Link href="/" passhref="true">
+                  info@example.com
+                </Link>
+              </li>
+            </Box>
+            <Box>
+              <li>
+                <Link href="Tel:+234 07034278995" passhref="true">
+                  +234 (0) 70 3427 8995
+                </Link>
+              </li>
+            </Box>
+          </Flex>
         </Box>
         <Box>
-          <ul>
-            <li>
-              <Link href="/access/login" passhref="true">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link href="/access/signin" passhref="true">
-                Register
-              </Link>
-            </li>
-          </ul>
+          <Flex>
+            <Box>
+              <li>
+                <Link href="/access/login" passhref="true">
+                  Login
+                </Link>
+              </li>
+            </Box>
+            <Box>
+              <li>
+                <Link href="/access/signin" passhref="true">
+                  Register
+                </Link>
+              </li>
+            </Box>
+          </Flex>
         </Box>
       </Flex>
-      <h1>Topnav</h1>
     </>
   );
 };
