6794d75c91da98516cb941b3291a0b50e9c7d681
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-26 08:48:33 +0100

    chores:added all the footer links

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -2,14 +2,55 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import action from "../../public/images/action.png";
+import { Flex, Box } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <>
-      <Link href="/">
-        <Image src={action} className="brand-logo" alt="brand Logo" />
-      </Link>
-      {/* <h1>footer</h1> */}
+      <Flex>
+        <Box>
+          <Link href="/">
+            <Image src={action} className="brand-logo" alt="brand Logo" />
+          </Link>
+          <p>
+            Neque porro quisquam est qui most dolorem quia enough impedit same
+            quo minus quod and maxime
+          </p>
+        </Box>
+        <Box>
+          <h2>Links</h2>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              Our Service
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              Company
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              Blog Post
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="nav-link" passhref="true">
+              Contact
+            </Link>
+          </li>
+        </Box>
+      </Flex>
     </>
   );
 };
