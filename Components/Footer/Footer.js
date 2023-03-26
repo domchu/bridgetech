af5e85ea0bc356929fd106ff3f77fbb1aab9f847
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-26 08:35:33 +0100

    chores:added the footer branding

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import action from "../../public/images/action.png";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <>
+      <Link href="/">
+        <Image src={action} className="brand-logo" alt="brand Logo" />
+      </Link>
+      {/* <h1>footer</h1> */}
+    </>
   );
 };
 
