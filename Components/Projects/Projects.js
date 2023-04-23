356a66ea3b5254c050b4fe402d179135b8b4c7f5
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-23 10:03:02 +0100

    chores:type one of the project items

--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+import Image from "next/image";
+import { HiArrowLongRight } from "react-icons/hi2";
 import SubHeading from "./../SubHeading";
+import project2 from "../../public/images/project2.jpg";
 
 const Projects = () => {
   return (
@@ -8,7 +13,17 @@ const Projects = () => {
       <Box className="w-container">
         <Box className="project-wrapper">
           <Box className="project-collection">
-            <img src="" alt="Digital marketing" loading="lazy" />
+            <Image src={project2} alt="Digital marketing" loading="lazy" />
+            <Box className="project__contents">
+              <h3>Digital Marketing</h3>
+              <p>
+                Neque porro quisquam est qui most dolorem quia enough impedit
+                same quo minus quod and maxime
+              </p>
+              <Link href="/">
+                <HiArrowLongRight />
+              </Link>
+            </Box>
           </Box>
         </Box>
       </Box>
