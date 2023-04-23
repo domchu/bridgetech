64645e2df287778d8b011902073d30c0565d2fa7
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-23 01:16:03 +0100

    styles the project sub heading

--- Components/SubHeading.js
+++ Components/SubHeading.js
@@ -5,23 +5,27 @@ import { Box } from "@chakra-ui/react";
 const SubHeading = () => {
   return (
     <>
-      <Box>
-        <h1>
-          Our <span className="sub_heading_span">Project</span>
-        </h1>
-        <p>
-          Neque porro quisquam est, qui quia lorem some golor sit amet, are more
-          consectetur sed
-        </p>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <Box className="separator">/</Box>
-          <li>
-            <Box>Projects</Box>
-          </li>
-        </ul>
+      <Box className="project-sub-container">
+        <Box className="project-sub-content">
+          <h1 className="project-title">
+            Our <span className="sub_heading_span">Project</span>
+          </h1>
+          <p>
+            Neque porro quisquam est, qui quia lorem some golor sit amet, are
+            more consectetur sed
+          </p>
+          <ul className="project-link">
+            <li className="project-nav-items">
+              <Link href="/" className="project-nav-link">
+                Home
+              </Link>
+            </li>
+            <Box className="separator">/</Box>
+            <li>
+              <Box className="project-link-text">Projects</Box>
+            </li>
+          </ul>
+        </Box>
       </Box>
     </>
   );
