ce25fe92b13755fcab7ac3e3197e62ed2dd17ad6
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-25 22:47:48 +0100

    chore:install embeded google map to the contact

--- Components/Map.js
+++ Components/Map.js
@@ -8,9 +8,20 @@ const Map = () => {
     <>
       <Box className="section-gap-130">
         <Box className="container w-container">
-          <Box>
-            {/* Map section */}
-            <Box className="contact-block"></Box>
+          {/* Map section */}
+          <Box className="contact-block">
+            <Box className="contact-map-block">
+              <Box className="map-container google-map">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7928.753336234345!2d3.1731800210065506!3d6.4738819333213105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b819c6fba15b1%3A0xacc3dbc2fa9d737a!2sKeyonavilleschool!5e0!3m2!1sen!2sng!4v1675896538770!5m2!1sen!2sng"
+                  // width="820px"
+                  // height="400px"
+                  loading="lazy"
+                  // referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </Box>
+            </Box>
+
             {/* contact section */}
             <Box className="contact-content-block">
               <Box className="section-block">
