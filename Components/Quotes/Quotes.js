2c2366221647bce2d54c79d2a0d7f34bbae0e771
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-27 01:44:24 +0100

    chore:added the quotes sub heading

---
+++ Components/Quotes/Quotes.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Quotes = () => {
+  return (
+    <>
+      <Box className="project-sub-container">
+        <Box className="w-container">
+          <Box className="project-block">
+            <Box className="project-sub-content">
+              <h1 className="project-title">
+                Get a <span className="sub_heading_span">Quotes</span>
+              </h1>
+              <p>
+                Neque porro quisquam est, qui quia lorem some golor sit amet,
+                are more consectetur sed
+              </p>
+              <ul className="project-link">
+                <li className="project-nav-items">
+                  <Link href="/" className="project-nav-link">
+                    Home
+                  </Link>
+                </li>
+                <Box className="separator">/</Box>
+                <li>
+                  <Box className="project-link-text">Quotes</Box>
+                </li>
+              </ul>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Quotes;
--- pages/pages/quotes.js
+++ pages/pages/quotes.js
@@ -3,6 +3,7 @@ import Head from "next/head"
 import Header from "../../Components/Header/Header";
 import Topnav from "../../Components/Topnav/Topnav";
 import Footer from "../../Components/Footer/Footer";
+import Quotes from "../../Components/Quotes/Quotes";
 
 const quotes = () => {
   return (
@@ -17,7 +18,8 @@ const quotes = () => {
       </Head>
       <Topnav />
       <Header />
-      <h1>Quotes</h1>
+      <Quotes />
+
       <Footer />
     </>
   );
