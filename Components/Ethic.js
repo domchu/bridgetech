cd9f51113d7bd175de274990aa4cbb3fa796f69f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-30 04:00:37 +0100

    chore:styled the ethic comp items

--- Components/Ethic.js
+++ Components/Ethic.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+import MeetPeople from "../public/images/work-proess-2.png";
 
 const Ethic = () => {
   return (
@@ -14,14 +16,45 @@ const Ethic = () => {
                 </h2>
                 <Box className="section-text">
                   Neque porro quisquam est, qui dolorem ipsum quia golor sit
-                                  amet
-                                  <br />
+                  amet
+                  <br />
                   ctetur, adipisci velit, sed eligendi optio cumque nihil
                   impedit
                 </Box>
               </Box>
             </Box>
-            <Box className="work-progress-block"></Box>
+            <Box className="work-progress-block">
+              <Box
+                className="single-work-progress-item"
+                id="w-node-_0e08f737-4d77-aa87-5e85-122aa03eaf97-44adaf1d"
+              >
+                <Box className="work-progress-icon-block">
+                  <Image
+                    src={MeetPeople}
+                    alt="Work Icon Image"
+                    className="work-progress-icon-image img"
+                    loading="lazy"
+                  />
+                </Box>
+                <Box
+                  className="work-progress-text"
+                  style={{ color: "rgb(2,62.138)" }}
+                >
+                  Meet the Client
+                </Box>
+                <Box
+                  className="work-progress-number-text"
+                  style={{ opacity: "0.3" }}
+                >
+                  01
+                </Box>
+                <Box
+                  className="work-progress-horizontal-lin-bottom"
+                  style={{}}
+                  id="special"
+                ></Box>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
