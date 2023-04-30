2c849ca31965d8e4814a626cbd3ddff0460a4365
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-04-30 01:57:31 +0100

    chore:Ethic title and texts

--- Components/Ethic.js
+++ Components/Ethic.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 
 const Ethic = () => {
   return (
     <>
-      <h1>Ethic</h1>
+      <Box className="work-progress wf-section">
+        <Box className="section-gap-bottom-130">
+          <Box className="container justify-center w-container">
+            <Box className="section-justify-center">
+              <Box className="section-block max-width-475 text-center">
+                <h2 className="section-title">
+                  How <span className="section-title-inner-style">we</span> work
+                </h2>
+                <Box className="section-text">
+                  Neque porro quisquam est, qui dolorem ipsum quia golor sit
+                                  amet
+                                  <br />
+                  ctetur, adipisci velit, sed eligendi optio cumque nihil
+                  impedit
+                </Box>
+              </Box>
+            </Box>
+            <Box className="work-progress-block"></Box>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
