1393fcefe27f517188dd8bc84fa109ad3a6081a9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-01 13:06:07 +0100

    chore:finished the service subheading

--- Components/Service/Service.js
+++ Components/Service/Service.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Service = () => {
   return (
     <>
-      <h1>Service</h1>
+      <Box className="project-sub-container">
+        <Box className="w-container">
+          <Box className="project-block">
+            <Box className="project-sub-content">
+              <h1 className="project-title">
+                Our <span className="sub_heading_span">Services</span>
+              </h1>
+              <p>
+                Neque porro quisquam est, qui quia lorem some golor sit amet,
+                are more consectetur sed
+              </p>
+              <ul className="project-link">
+                <li className="project-nav-items">
+                  <Link href="/" className="project-nav-link">
+                    Home
+                  </Link>
+                </li>
+                <Box className="separator">/</Box>
+                <li>
+                  <Box className="project-link-text">Services</Box>
+                </li>
+              </ul>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
