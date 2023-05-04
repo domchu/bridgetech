ab0af061a536b23f0938cf696f3e9a679680fe74
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-04 11:39:07 +0100

    chore:done with how it work part

--- Components/Single-Services/Singleservices.js
+++ Components/Single-Services/Singleservices.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Benefit from "../../public/images/service-benefit.jpg";
 import ServiceImage from "../../public/images/service-main.jpg";
+import ServiceWork from "../../public/images/service-6.jpg";
 
 const Singleservices = () => {
   return (
@@ -60,7 +61,59 @@ const Singleservices = () => {
                   />
                 </Box>
               </Box>
-              <Box className="service-how-to-block"></Box>
+              <Box className="service-how-to-block">
+                <Box className="service-how-to-image-block">
+                  <Image
+                    src={ServiceWork}
+                    alt="Software Development"
+                    loading="lazy"
+                    className="image-cover img"
+                  />
+                </Box>
+                <Box className="service-how-to-content">
+                  <h2 className="section-title">How it works</h2>
+                  <Box className="w-richtext">
+                    <p>
+                      We are <strong>Optitech</strong> provide the best quality
+                      <Link href="https://www.example.com" target="_blank">
+                        It solution
+                      </Link>
+                      neque porro quisquam est qui dolorem ipsum quia golor sit
+                      amet, conse ctetur, adipisci velit, sed eligendi optio
+                      cumque nihil impedit quo minus id quod maxime placeat
+                    </p>
+                  </Box>
+                  <Box className="inner-quote-block">
+                    <Box className="service-quote w-richtext">
+                      <p>
+                        IT Management provide the most service neque porro
+                        quisquam est qui dolorem ipsum quia golor sit amet,
+                        conse ctetur, adipisci velit
+                      </p>
+                      <p>
+                        IT Management provide the most service neque porro
+                        quisquam est qui dolorem ipsum quia golor sit amet,
+                        conse ctetur, adipisci velit
+                      </p>
+                    </Box>
+                    <Box className="service-quote-border-left"></Box>
+                  </Box>
+                  <Box className="w-text">
+                    <p>
+                      The main way to solve the the best quality
+                      <Link href="https://www.example.com">It solution</Link>
+                      neque porro quisquam qui dolorem ipsum quia golor sit
+                      amet, conse ctetur, adipisci velit, sed eligendi optio
+                      cumque nihil impedit quo minus id quod maxime placeat
+                    </p>
+                    <p>
+                      Great deal dolorem ipsum quia golor sit amet, conse
+                      ctetur, adipisci velit, sed optio cumque nihil impedit quo
+                      minus id quod maxime placeat
+                    </p>
+                  </Box>
+                </Box>
+              </Box>
               <Box className="service-benefit-block">
                 <Box className="service-benefit-content">
                   <h2 className="section-title">Benefits</h2>
