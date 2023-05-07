e8f43b5205336286b57c9db30468cf882994dc0e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-07 15:55:27 +0100

    chore:done with the dropdown menu

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,5 +1,5 @@
 // import React, { useState, useRef, useEffect } from "react";
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -9,20 +9,20 @@ import Brand from "../../public/images/brandlogo-blue.png";
 import Dropdown from "../../public/images/arrow-down.svg";
 
 const Navbar = () => {
-  // active pages route
+  // ACTIVE PAGES LINK
   const router = useRouter();
   const currentRoute = router.pathname;
   return (
     <>
       <nav>
         <Box className=" header-navbar-block">
-          {/* the logo section */}
+          {/* THE BRAND LOGO SECTION */}
           <Box className=" logo-block">
             <Link href="/">
               <Image src={Brand} className="brand-logo" alt="brand Logo" />
             </Link>
           </Box>
-          {/* the navbar section */}
+          {/* THE NAVBAR SECTION */}
           <Box className="header-menu-wrapper">
             <div className="nav-menu w-nav-menu">
               <ul className="links-container-nav">
@@ -36,7 +36,7 @@ const Navbar = () => {
                     Home
                   </Link>
                 </li>
-                <li>
+                <Box>
                   <Box
                     className="dropdown w-dropdown"
                     data_hover="fales"
@@ -53,7 +53,7 @@ const Navbar = () => {
                       tabIndex={0}
                     >
                       <Box id="nav-link">Pages</Box>
-                      <img
+                      <Image
                         src={Dropdown}
                         alt="Arrow Down Icon"
                         loading="lazy"
@@ -64,72 +64,87 @@ const Navbar = () => {
                       className="nav-dropdown-list w-dropdown-list w--open"
                       aria-labelledby="w-dropdown-toggle-0"
                     >
-                      <Link
-                      
-                        href="/pages/aboutus"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={currentRoute == "/" ? "active" : ""}
-                        tabIndex={0}
-                      >
-                        About Us
-                      </Link>
-                      <Link
-                        href="/pages/team"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={
-                          currentRoute == "/pages/team" ? "active" : ""
-                        }
-                        tabIndex={0}
-                      >
-                        Team
-                      </Link>
-                      <Link
-                        href="/access/signin"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={
-                          currentRoute == "/access/signin" ? "active" : ""
-                        }
-                        tabIndex={0}
-                      >
-                        Signup
-                      </Link>
-                      <Link
-                        href="/access/login"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={
-                          currentRoute == "/access/login" ? "active" : ""
-                        }
-                        tabIndex={0}
-                      >
-                        Login
-                      </Link>
-                      <Link
-                        href="/pages/getquotes"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={
-                          currentRoute == "/pages/getquotes" ? "active" : ""
-                        }
-                        tabIndex={0}
-                      >
-                        Get A Quote
-                      </Link>
-                      <Link
-                        href="/pages/faq"
-                        id="nav-dropdown-link w-nav-link"
-                        passhref="true"
-                        className={currentRoute == "/pages/faq" ? "active" : ""}
-                        tabIndex={0}
-                      >
-                        Faq
-                      </Link>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/pages/aboutus"
+                          id="nav-dropdown-link "
+                          passhref="true"
+                          className={
+                            currentRoute == "/pages/aboutus" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          About Us
+                        </Link>
+                      </li>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/pages/team"
+                          id="nav-dropdown-link"
+                          passhref="true"
+                          className={
+                            currentRoute == "/pages/team" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          Team
+                        </Link>
+                      </li>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/access/signin"
+                          id="nav-dropdown-link "
+                          passhref="true"
+                          className={
+                            currentRoute == "/access/signin" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          Signup
+                        </Link>
+                      </li>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/access/login"
+                          id="nav-dropdown-link "
+                          passhref="true"
+                          className={
+                            currentRoute == "/access/login" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          Login
+                        </Link>
+                      </li>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/pages/faq"
+                          id="nav-dropdown-link "
+                          passhref="true"
+                          className={
+                            currentRoute == "/pages/faq" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          Faq
+                        </Link>
+                      </li>
+                      <li className="w-nav-link nav-dropdown-link">
+                        <Link
+                          href="/pages/getquotes"
+                          id="nav-dropdown-link "
+                          passhref="true"
+                          className={
+                            currentRoute == "/pages/getquotes" ? "active" : ""
+                          }
+                          tabIndex={0}
+                        >
+                          Get A Quote
+                        </Link>
+                      </li>
                     </Box>
                   </Box>
-                </li>
+                </Box>
                 <li>
                   <Link
                     href="/services/service-home"
@@ -174,8 +189,8 @@ const Navbar = () => {
                 </li>
               </ul>
             </div>
-            {/* the Quotes section */}
-            <Box className="header-event-button-group">
+            {/* THE QUOTES SECTION AT THE NAVBAR */}
+            <Box className="header-event-button-group quotes-container">
               <li className="quote-primary small">
                 <Link
                   href="/pages/quotes"
@@ -195,22 +210,4 @@ const Navbar = () => {
 
 export default Navbar;
 
-{
-  /* <Link
-href='/about'
-className={currentRoute === '/about' ? styles.active : styles.nonActive}
->
-About
-</Link> */
-}
 
-/* Specific styles for non-active links */
-// .nonActive {
-//   color: white;
-// }
-
-/* Specific styles for active links */
-// .active {
-//   color: black;
-//   background: orange;
-// }
