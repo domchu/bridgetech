05f17248fcee1deb89e2cd7db7223dc32a89a851
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-09 16:54:25 +0100

    chore:added the subheading to the login page

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Login = () => {
   return (
     <>
-      <h1>Login Detail</h1>
+      <Box className="project-sub-container">
+        <Box className="w-container">
+          <Box className="project-block">
+            <Box className="project-sub-content">
+              <h1 className="project-title">
+                Login <span className="sub_heading_span">Page</span>
+              </h1>
+              <p>
+                Neque porro quisquam est, qui quia lorem some golor sit amet,
+                are more consectetur sed
+              </p>
+              <ul className="project-link">
+                <li className="project-nav-items">
+                  <Link href="/" className="project-nav-link">
+                    Home
+                  </Link>
+                </li>
+                <Box className="separator">/</Box>
+                <li>
+                  <Box className="project-link-text">Login</Box>
+                </li>
+              </ul>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
