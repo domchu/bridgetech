7f695c60aaea330513e3235f55b3e8cc7a9b6a49
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-10 00:01:00 +0100

    chore:enter all the inputs login form structure

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -31,6 +31,70 @@ const Login = () => {
           </Box>
         </Box>
       </Box>
+
+      {/* LOGIN FORM */}
+      <Box className="login-section wf-section">
+        <Box className="section-gap-130">
+          <Box className="container w-container">
+            <Box className="login-block">
+              <Box className="login-form-area">
+                <Box className="section-block">
+                  <h2 className="section-title max-width-442">
+                    Login to
+                    <span className="section-title-inner-style">
+                      Bridgetech
+                    </span>
+                  </h2>
+                  <Box className="section-text">
+                    Neque porro quisquam est, qui dolorem ipsum quia golor sit
+                    amet, conse ctetur, adipisci velit, sed eligendi optio
+                    cumque nihil impedit quo minus id quod maxime
+                  </Box>
+                </Box>
+                <Box className="section-form-block w-form">
+                  <form
+                    action=""
+                    id="wf-form-login-form"
+                    className="login-form"
+                    data-name="Login Form"
+                    method="post"
+                    name="wf-form-login-form"
+                    aria-label="Login Form"
+                  >
+                    <input
+                      type="email"
+                      className="default-input-field w-input"
+                      maxLength="256"
+                      name="email"
+                      id="Login Email"
+                      data-name="Email"
+                      placeholder="Enter Email*"
+                      required={true}
+                    />
+                    <input
+                      type="password"
+                      className="default-input-field w-input"
+                      maxLength="256"
+                      name="Password"
+                      id="Login Password"
+                      data-name="Password"
+                      placeholder="Enter Password*"
+                      required={true}
+                    />
+                    <input
+                      type="submit"
+                      value="Login"
+                      className="primary-button w-button"
+                      data-wait="Please Wait"
+                    />
+                  </form>
+                </Box>
+              </Box>
+              <Box className="login-image-box"></Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
