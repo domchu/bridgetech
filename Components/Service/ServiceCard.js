f3dee0c941d5a8b43e9297815fb44b499d18942b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-10 12:38:17 +0100

    chore:service data as json

---
+++ Components/Service/ServiceCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServiceCard = () => {
+  return (
+    <>
+      <h1>ServiceCard</h1>
+    </>
+  );
+};
+
+export default ServiceCard;
---
+++ Components/Service/ServiceData.js
@@ -0,0 +1,62 @@
+const DataServ = [
+  {
+    image: "/images/project7.jpg",
+    title: " Mobile App",
+    urlPath: "/services/mobile-app",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project5.jpg",
+    title: " Website Development",
+    urlPath: "/services/web-development",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project10.jpg",
+    title: "Web Design Training",
+    urlPath: "/services/web-design-training",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project2.jpg",
+    title: " UI / UX Design",
+    urlPath: "/services/ui-ux",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project7.jpg",
+    title: " Graphics Design",
+    urlPath: "/services/graphics-design",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project3.jpg",
+    title: "Software Development",
+    urlPath: "/services/mobile-app",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project1.jpg",
+    title: "  IT Setup & Management",
+    urlPath: "/services/it-setup-mgmt",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project5.jpg",
+    title: " Branding & SEO",
+    urlPath: "/services/mobile-app",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project10.jpg",
+    title: " Hardware/network Engr.",
+    urlPath: "/services/mobile-app",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+  {
+    image: "/images/project7.jpg",
+    title: " Human Resources(HR)",
+    urlPath: "/services/mobile-app",
+    text: " Neque porro quisquam est qui most dolorem quia enough impedit same quo minus quod and maxime ",
+  },
+];
