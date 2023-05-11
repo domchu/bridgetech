106289a93eecf55606201810e1c5313eba313aff
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-11 17:56:26 +0100

    chore:added  more data to the testimonial json

--- Components/Testimonails/Testimonials.js
+++ Components/Testimonails/Testimonials.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
-const Testimonials = () => {
+const Testimonial = () => {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  //* useEffect function to negative numbers.
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  //* useEffect for the auto slider.
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 7000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <div>
-      <h1>testionals slider</h1>
-    </div>
-  )
-}
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> TESTIMONIALS
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, title, quote, name } = person;
 
-export default Testimonials
+          //* conditions for the slide.
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          //* mapping the json/data to the dom.
+          return (
+            <article className={position} key={id}>
+              {/* <img
+                src={image}
+                alt={name}
+                className="person-img"
+                loading="lazy"
+              /> */}
+              <h4>{name}</h4>
+              <p className="title">{title} </p>
+              <p className="text">{quote} </p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+
+        {/* the prev and next buttons */}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
--- Components/Testimonails/data.js
+++ Components/Testimonails/data.js
@@ -1,5 +1,49 @@
 
-const data = [
-    {},
-]
-export default data
+const TestimonialData = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Mrs Uzoma",
+    title: "school Parent",
+    quote:
+      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Mrs Uzoma",
+    title: "school Parent",
+    quote:
+      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Mrs Uzoma",
+    title: "school Parent",
+    quote:
+      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Mrs Uzoma",
+    title: "school Parent",
+    quote:
+      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+  },
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Mrs Uzoma",
+    title: "school Parent",
+    quote:
+      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+  },
+];
+export default TestimonialData;
