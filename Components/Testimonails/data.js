5f993ad4251bf268e767d190f59673037f7c4d84
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-12 10:40:48 +0100

    chore:added one client testimonial data

--- Components/Testimonails/data.js
+++ Components/Testimonails/data.js
@@ -2,12 +2,11 @@
 const TestimonialData = [
   {
     id: 1,
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    name: "Mrs Uzoma",
-    title: "school Parent",
+    image: "/images/full-stack.jpg",
+    name: "Zenefer Smith",
+    title: "Keyonaville HeadTeacher",
     quote:
-      "I thank keyonaville  for calling us on phone to check on our children also, to remind us and to find out if there's any challenges when our kids are yet to be picked up from the school. Thank you for your care. ",
+      "Best service for us amd I am very so satisfy with them is the best way a cases perfectly simple and easy ",
   },
   {
     id: 2,
