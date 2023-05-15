8eaa5396bced6c82bfb33a4dcbe8040e18d44c32
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-15 13:15:54 +0100

    chore:added the pagination structure

--- Components/Projects/ProjectCard.js
+++ Components/Projects/ProjectCard.js
@@ -40,6 +40,38 @@ const ProjectCard = () => {
           </Box>
         );
       })}
+
+      <Box className="pagination-container">
+        <Box aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#">
+                Previous
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                2
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                Next
+              </a>
+            </li>
+          </ul>
+        </Box>
+      </Box>
     </>
   );
 };
