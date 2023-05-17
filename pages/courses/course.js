42dfe00397f525f72dd59aa0e1cd11bb7e7e8088
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-18 00:37:11 +0100

    chore:added the head to the course page

---
+++ pages/courses/course.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const course = () => {
+  return (
+    <>
+      <Head>
+        <title>Bridgekode | Courses</title>
+        <meta
+          name="courses"
+          content="you can select from our wide range of courses"
+        />
+        <link rel="icon" href="/logo-blue.png" size="64x64" />
+      </Head>
+    </>
+  );
+};
+
+export default course;
