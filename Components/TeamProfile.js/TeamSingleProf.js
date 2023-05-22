07de24e2555456db520c986d2c67f0356f49533a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-23 00:15:02 +0100

    chore:import all the crucial files to team ceo

--- Components/TeamProfile.js/TeamSingleProf.js
+++ Components/TeamProfile.js/TeamSingleProf.js
@@ -24,7 +24,7 @@ const TeamSingleProf = () => {
                 </li>
                 <Box className="separator">/</Box>
                 <li className="project-nav-items">
-                  <Link href="/" className="project-nav-link">
+                  <Link href="/team/president" className="project-nav-link">
                     Team
                   </Link>
                 </li>
--- pages/team/president.js
+++ pages/team/president.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Head from "next/head";
+import Topnav from "../../Components/Topnav/Topnav";
+import Header from "../../Components/Header/Header";
+import Testimonial from "../../Components/Testimonails/Testimonial";
+import Footer from "../../Components/Footer/Footer";
 
 const president = () => {
   return (
     <>
-      <head>president</head>
+      <Head>
+        <title>Bridgekode | Contact Us</title>
+        <meta
+          name="courses"
+          content="you can select from our wide range of courses"
+        />
+        <link rel="icon" href="/logo-blue.png" size="64x64" />
+      </Head>
+      <Topnav />
+      <Header />
+      <Testimonial />
+      <Footer />
     </>
   );
 };
