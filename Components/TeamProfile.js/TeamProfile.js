3ed4b4027e3ebeb8ff7cd95375bc0374f88f2445
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-23 22:54:58 +0100

    chore:style the team profile

--- Components/TeamProfile.js/TeamProfile.js
+++ Components/TeamProfile.js/TeamProfile.js
@@ -16,7 +16,12 @@ const TeamProfile = () => {
           <Box className="container w-container">
             <Box className="team-details-block">
               <Box className="team-details-image-block">
-                <Image src={Man} alt="A man wearing suite" loading="lazy" />
+                <Image
+                  src={Man}
+                  alt="A man wearing suite"
+                  loading="lazy"
+                  className="image-responsie team-large-image"
+                />
                 <Box className="team-details-image-shadow"></Box>
                 <Box className="team-details-social-block">
                   <Link
@@ -52,6 +57,35 @@ const TeamProfile = () => {
               {/* CONTENTS SIDE OF THE TEAM */}
               <Box className="team-details-content-block">
                 <Box className="team-details-greeting-text">Hi there!</Box>
+                <Box className="team-details-name">
+                  <Box>I am</Box>
+                  <Box>Luther Mathuis</Box>
+                </Box>
+                <Box className="team-details-job-title">CEO, President</Box>
+                <Box className="contact-info-title">Biography</Box>
+                <Box className="team-biography-text w-richtext">
+                  <p>
+                    I am expert on <strong>bridgekode</strong> provide the best
+                    quality It solution neque porro quisqu qui dolorem ipsum
+                    quia golor sit amet, conse ctetur, adipisci velit, sed
+                    eligendi optio cumque nihil impedit quo minus id quod maxime
+                    placeat most of the a people want to lear IT related
+                    solution is very great reason
+                  </p>
+                </Box>
+                <Box className="contact-info-title">Achievement</Box>
+                <Box className="achivement-text">Best Entrepreneur - 2020</Box>
+                <Box className="team-contact-details-info">
+                  <Box className="contact-info-title">Contact Info</Box>
+                  <Box className="contact-info-small-title">Address</Box>
+                  <Box className="contact-info-text">
+                    3947 Grove Avenue Oklahoma City, OK 73129
+                  </Box>
+                  <Box className="contact-info-small-title">Phone</Box>
+                  <Box className="contact-info-text">580-366-0345</Box>
+                  <Box className="contact-info-small-title">Email</Box>
+                  <Box className="contact-info-text">info@example.com</Box>
+                </Box>
               </Box>
             </Box>
           </Box>
