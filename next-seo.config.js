79ab58599e0dd473b0e0216424190ddadf4fc40c
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-24 21:27:31 +0100

    chore:added the open graph

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.bridgekode.com",
+    siteName: "bridgekode",
+    images: [
+      {
+        url: "https://www.bridgekode.com.ng/logo-blue.png",
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@bridgekode",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
