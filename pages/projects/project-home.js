f1186a91bce0bb16c07dd8921278b1bdcf9e3a9e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-27 00:43:17 +0100

    chore:added the subheading to the project details

---
+++ pages/projects/project-home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Topnav from "../../Components/Topnav/Topnav";
+import Footer from "../../Components/Footer/Footer";
+import Testimonial from "../../Components/Testimonails/Testimonial";
+import Projects from "../../Components/Projects/Projects";
+import { DefaultSeo } from "next-seo";
+import SEO from "../../next-seo.config";
+
+const projecthome = () => {
+  return (
+    <>
+      <DefaultSeo
+        title="project | Bridgekode"
+        description="We offers the various IT services such as mobile apps, design websites, HR services, SEO, training,IT setup, IT management etc"
+        {...SEO}
+      />
+      <Topnav />
+      <Header />
+      <Projects />
+      <Testimonial />
+      <Footer />
+    </>
+  );
+};
+
+export default projecthome;
