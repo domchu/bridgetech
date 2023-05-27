825d6bcbb6e58eb7d67f38d7e139209aa779479e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-27 03:52:43 +0100

    chore:styles the project single details

--- Components/Projects/SingleProject.js
+++ Components/Projects/SingleProject.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Box } from "@chakra-ui/react";
 import WebImage from "../../public/images/project2.jpg";
 import WebIm from "../../public/images/service-benefit.jpg";
+import ladyImage from "../../public/images/ladyimage.png";
 
 const SingleProject = () => {
   return (
@@ -26,7 +27,7 @@ const SingleProject = () => {
                   src={WebImage}
                   alt="Web Image"
                   loading="lazy"
-                  className="image-resposive"
+                  className="image-responsive img"
                 />
               </Box>
               <h2 className="project-details-subtile-title">
@@ -52,12 +53,64 @@ const SingleProject = () => {
                   </Box>
                 </Box>
               </Box>
-                          <Box className="how-to-block">
-                              
+              <Box className="how-to-block">
+                <Box className="how-to-left-block">
+                  <h2 className="project-details-subtile-title">
+                    How this Project works
+                  </h2>
+                  <Box className="w-richtext">
+                    <p>
+                      We are <strong>Optitech</strong> provide the best quality
+                      It solution neque porro quisquam est qui dolorem ipsum
+                      quia golor sit amet, conse ctetur, adipisci velit, sed
+                      eligendi optio cumque nihil impedit quo minus id quod
+                      maxime placeat
+                    </p>
+                  </Box>
+                  <Box className="inner-quote-block">
+                    <Box className="w-richtext">
+                      <p>
+                        IT Consultancy provide the most service neque porro
+                        quisquam est qui dolorem ipsum quia golor sit amet,
+                        conse ctetur, adipisci velit
+                      </p>
+                      <p>
+                        IT Consultancy provide the most service neque porro
+                        quisquam est qui dolorem ipsum quia golor sit amet,
+                        conse ctetur, adipisci velit
+                      </p>
+                      <Box className="inner-quote-border-left"></Box>
+                    </Box>
+                  </Box>
+                  <Box className="w-richtext">
+                    <p>
+                      The main way to solve the the best quality It solution
+                      neque porro quisquam qui dolorem ipsum quia golor sit
+                      amet, conse ctetur, adipisci velit, sed eligendi optio
+                      cumque nihil impedit quo minus id quod maxime placeat
+                    </p>
+                    <p>
+                      Great deal dolorem ipsum quia golor sit amet, conse
+                      ctetur, adipisci velit, sed optio cumque nihil impedit quo
+                      minus id quod maxime placeat optio cumque nihil impedit
+                      quo minus id quod maxime placeat
+                    </p>
+                  </Box>
+                </Box>
+                <Box className="how-to-right-block">
+                  <Box className="how-to-image-block">
+                    <Image
+                      src={ladyImage}
+                      alt="A lady with computer"
+                      loading="lazy"
+                      className="image-responive img"
+                    />
+                  </Box>
+                </Box>
               </Box>
               <Box className="project-result-block">
-                <h2 className="project-details-subtile-title">
-                  Project Information
+                <h2 className="project-details-subtile-title ">
+                  Project Result & Benefits of Project
                 </h2>
                 <Box className="w-richtext">
                   <p>
@@ -99,29 +152,30 @@ const SingleProject = () => {
                   />
                 </Box>
                 <Box className="project-list-content">
-                  <Box className="project-list-subtile">
+                  <Box className="project-list-subtitle">
                     In IT Consultancy Service we cover most of the IT problems
                     that your company faces and we try to solve it.
                   </Box>
                   <Box className="w-richtext">
                     <p>
-                      We are Optitech provide the best quality It solution neque
-                      porro quisquam est qui dolorem ipsum quia golor sit amet,
-                      conse ctetur, adipisci velit, sed eligendi optio cumque
-                      nihil impedit quo minus id quod maxime placeat
+                      We are <strong>Optitech</strong> provide the best quality
+                      It solution neque porro quisquam est qui dolorem ipsum
+                      quia golor sit amet, conse ctetur, adipisci velit, sed
+                      eligendi optio cumque nihil impedit quo minus id quod
+                      maxime placeat
                     </p>
                     <ul role="list">
-                      <li>
+                      <li className="li">
                         IT Management provide the most service neque porro
                         quisquam est qui dolorem ipsum quia golor sit amet,
                         conse ctetur, adipisci velit
                       </li>
-                      <li>
+                      <li className="li">
                         IT Management provide the most service neque porro
                         quisquam est qui dolorem ipsum quia golor sit amet,
                         conse ctetur, adipisci velit
                       </li>
-                      <li>
+                      <li className="li">
                         IT Management provide the most service neque porro
                         quisquam est qui dolorem ipsum quia golor sit amet,
                         conse ctetur, adipisci velit
