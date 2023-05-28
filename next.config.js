45c5e0701a3b7a5fd827e9a8b6bc9b63d3d8be86
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-05-28 14:39:26 +0100

    chore:images unoptimization

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 
-// module.exports = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname:
-//           "assets.website-files.com/6330105a320e0e0ed5adaf1c/63536b582cf25003d158f7c8_bannerimage.png.com",
-//         port: "",
-//         pathname: "/account123/**",
-//       },
-//     ],
-//   },
-// };
+
