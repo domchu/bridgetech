1150bbf2f42ae7a51965746cd51f0de63622f580
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-05 20:58:36 +0100

    chore:included the subheading to course page

--- Components/Courses/SingleCourse.js
+++ Components/Courses/SingleCourse.js
@@ -2,19 +2,28 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "next/image";
+// import SubHeading from "../../Components/SubHeading";
 import project2 from "../../public/images/project10.jpg";
 
 const SingleCourse = () => {
   return (
     <>
+      {/* SUBHEADING */}
+      {/* <SubHeading /> */}
+
       <Box classname="course-section wf-section">
         <Box className="section-gap-bottom-130">
           <Box className="container w-container">
             <Box className="course-section-block">
               <Box className="course-block-text-left">
                 <Box className="section-block">
-                  <p className="section-title-inner-style" id="starter"> For Starters</p>
-                  <h2 className="section-title">Frontend Development Bootcamp</h2>
+                  <p className="section-title-inner-style" id="starter">
+                    {" "}
+                    For Starters
+                  </p>
+                  <h2 className="section-title">
+                    Frontend Development Bootcamp
+                  </h2>
                 </Box>
                 <Box className="section-text">
                   Go from zero knowledge to being able to build your technology
@@ -23,8 +32,7 @@ const SingleCourse = () => {
                   mastered a specialization and learn the art of effective
                   collaboration to solve real world problems.
                 </Box>
-                
-            
+
                 <Box>
                   <Link
                     href="/access/form"
--- pages/courses/branding.js
+++ pages/courses/branding.js
@@ -3,9 +3,11 @@ import Topnav from "../../Components/Topnav/Topnav";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Testimonial from "../../Components/Testimonails/Testimonial";
+import SingleCourse from "../../Components/Courses/SingleCourse";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo.config";
-import SingleCourse from "../../Components/Courses/SingleCourse";
 
 const branding = () => {
   return (
@@ -17,6 +19,41 @@ const branding = () => {
       />
       <Topnav />
       <Header />
+      <Box className="project-sub-container">
+        <Box className="w-container">
+          <Box className="project-block">
+            <Box className="project-sub-content">
+              <h1 className="project-title">
+                Course <span className="sub_heading_span">Details</span>
+              </h1>
+              <p>
+                The underlisted project was carried out by bridgekode, completed
+                and delivered to the client.
+              </p>
+              <ul className="project-link">
+                <li className="project-nav-items">
+                  <Link href="/" className="project-nav-link">
+                    Home
+                  </Link>
+                </li>
+                <Box className="separator">/</Box>
+                <li className="project-nav-items">
+                  <Link
+                    href="/projects/project-home"
+                    className="project-nav-link"
+                  >
+                    Cousre
+                  </Link>
+                </li>
+                <Box className="separator">/</Box>
+                <li>
+                  <Box className="project-link-text">Branding</Box>
+                </li>
+              </ul>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
       <SingleCourse />
       <Testimonial />
       <Footer />
