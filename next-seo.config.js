837ac2a683b5aae3e8e9534e3f3737644bead12e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-06 08:30:04 +0100

    chore:change d og image, height, width on twitter

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,12 @@
 export default {
   openGraph: {
     type: "website",
-    locale: "en_IE",
+    locale: "en_US",
     url: "https://www.bridgekode.com",
     siteName: "bridgekode",
     images: [
       {
-        url: "https://www.bridgekode.com/logo-blue.png",
+        url: "https://www.bridgekode.com/bridgekode-logo.png",
         width: 1200,
         height: 630,
         alt: "Og Image Alt",
@@ -17,7 +17,9 @@ export default {
   twitter: {
     handle: "@bridgekode",
     site: "@site",
-    images: "",
+    images: "https://www.bridgekode.com/bridgekode-logo.png",
+    width: 1280,
+    height: 640,
     cardType: "summary_large_image",
   },
 };
