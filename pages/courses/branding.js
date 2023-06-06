ec4341795ee8e13965462b76a0ea079e2f66e926
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-06 13:49:15 +0100

    chore:turn course subheading into reuseable comp.

--- pages/courses/branding.js
+++ pages/courses/branding.js
@@ -4,8 +4,7 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Testimonial from "../../Components/Testimonails/Testimonial";
 import SingleCourse from "../../Components/Courses/SingleCourse";
-import { Box } from "@chakra-ui/react";
-import Link from "next/link";
+import CoursesSubHeading from "./../../Components/CoursesSubHeading";
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo.config";
 
@@ -19,38 +18,7 @@ const branding = () => {
       />
       <Topnav />
       <Header />
-      <Box className="project-sub-container">
-        <Box className="w-container">
-          <Box className="project-block">
-            <Box className="project-sub-content">
-              <h1 className="project-title">
-                Course <span className="sub_heading_span">Details</span>
-              </h1>
-              <p>
-                Yes! You are on the right pathway, one step at a time. Your
-                career path to finacial stability begin here and now.
-              </p>
-              <ul className="project-link">
-                <li className="project-nav-items">
-                  <Link href="/" className="project-nav-link">
-                    Home
-                  </Link>
-                </li>
-                <Box className="separator">/</Box>
-                <li className="project-nav-items">
-                  <Link href="/courses/course" className="project-nav-link">
-                    Cousre
-                  </Link>
-                </li>
-                <Box className="separator">/</Box>
-                <li>
-                  <Box className="project-link-text">Branding</Box>
-                </li>
-              </ul>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
+      <CoursesSubHeading text="Branding" />
       <SingleCourse />
       <Testimonial />
       <Footer />
