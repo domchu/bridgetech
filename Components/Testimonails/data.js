2f8d80edd2c791a4189cec21df4e6af65600081d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-09 21:34:20 +0100

    chore:rearrange the slider data 4 testimonial

--- Components/Testimonails/data.js
+++ Components/Testimonails/data.js
@@ -2,14 +2,6 @@
 const TestimonialData = [
   {
     id: 1,
-    image: "/images/senwrita.jpg",
-    name: "Ekeh Esther Uchechi",
-    title: "Keyonaville Sch. HeadTeacher",
-    quote:
-      "I had a fantastic experience working with Bridgekode on my website development project. They were highly professional, attentive to detail, and delivered a stunning website that exceeded my expectations. I highly recommend Bridgekode for web development services. ",
-  },
-  {
-    id: 2,
     image: "/images/simon.jpg",
     name: "Odey Simon",
     title: "Pastor, City of Signs & Wonders Assembly.",
@@ -17,15 +9,16 @@ const TestimonialData = [
       "Creativity and excellent tech expertise of the team in combination with the transparency of working process make bridgekode one of the best company to work with. ",
   },
   {
-    id: 3,
-    image: "/images/full-stack.jpg",
-    name: "Muhammad Habeebulah",
-    title: "Web Developer",
+    id: 2,
+    image: "/images/senwrita.jpg",
+    name: "Ekeh Esther Uchechi",
+    title: "Keyonaville Sch. HeadTeacher",
     quote:
-      "Extremely fast and always available for any emergency issue. Always trying to understand the problem and propose the best solution possible ",
+      "I had a fantastic experience working with Bridgekode on my website development project. They were highly professional, attentive to detail, and delivered a stunning website that exceeded my expectations. I highly recommend Bridgekode for web development services. ",
   },
+
   {
-    id: 4,
+    id: 3,
     image: "/images/sleek.jpg",
     name: "Olatunde Sleek",
     title: "CEO Florintech Computer College",
@@ -33,12 +26,20 @@ const TestimonialData = [
       "Although they are a small team, their project management reflects the excellence of a larger company. Their team delivers high-quality results, attentive service, and reasonable pricing. ",
   },
   {
-    id: 5,
+    id: 4,
     image: "/images/chineyre.jpg",
     name: "Onuabueke Fidelia",
     title: "Clearing & Forwarding(Shipping)",
     quote:
       "Excellent responsiveness and focus on customer needs. Work is done by the book, when there is a crisis they respond rapidly. I can only recommend their services wholeheartedly.  ",
+  },
+  {
+    id: 5,
+    image: "/images/richcode.jpg",
+    name: "Ogada Stanley",
+    title: "Senior Software Engineer",
+    quote:
+      "I know Mr Dominic(Bridgekode CEO) as an over-thinking fellow, he never rests until he beats the problem. His a great programmer, mathematician, mentor & tutor. Though he's a master in diverse areas yet he is willing to still learn & be better.",
   },
   {
     id: 6,
@@ -47,6 +48,14 @@ const TestimonialData = [
     title: "Guidance counselor",
     quote:
       "I appreciated that the cost of the project turned out to be smaller than what we expected because they made some very good suggestions. They are very pleasant to work with.  ",
+  },
+  {
+    id: 7,
+    image: "/images/full-stack.jpg",
+    name: "Muhammad Habeebulah",
+    title: "Web Developer",
+    quote:
+      "Extremely fast and always available for any emergency issue. Always trying to understand the problem and propose the best solution possible ",
   },
 ];
 export default TestimonialData;
