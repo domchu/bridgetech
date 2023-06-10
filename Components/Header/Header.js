7876188a96836c3ccc7804b05c2cd1dc981c82b4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-10 15:30:57 +0100

    chore:fixed mobile toggle

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -246,7 +246,7 @@ const Navbar = () => {
                 />
               </Box>
               {/* MOBILE VIEW OF THE BRIDGEKODE */}
-              <Box className="" style={{ mobile: mobile ? "block" : "none" }}>
+              <Box className="" style={{ display: mobile ? "block" : "none" }}>
                 <MobileView />
               </Box>
               <Box>
