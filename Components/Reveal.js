90a0b1b7e88a1ebfc9d3460d3e1cdfa358796848
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-11 17:06:54 +0100

    chore:added the animation to the about us section

--- Components/Reveal.js
+++ Components/Reveal.js
@@ -12,6 +12,7 @@ const Reveal = ({ children }) => {
           }}
           initial="hidden"
           animate="visible"
+          transition={{ duration: 0.5, delay: 0.25 }}
         >
           {children}
         </motion.div>
