0090ddf8195899320688f995c9a3f2bbc3c02a8f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-11 15:29:20 +0100

    chore: install framer motion to the app

--- Components/About/About.js
+++ Components/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box } from "@chakra-ui/react";
-import Image from "next/image";
 import Link from "next/link";
 import TeamCard from "./../Team/TeamCard";
 import WhyBridgetech from "../WhyBridgetech";
@@ -144,22 +143,6 @@ const About = () => {
         </Box>
       </Box>
 
-      {/*CSS ANIMATED BACKGROUND  */}
-      {/* <Box className="area">
-        <ul className="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </Box> */}
-      {/* end of animated */}
       {/* WHY BRIDGEKODE  */}
       <WhyBridgetech />
 
