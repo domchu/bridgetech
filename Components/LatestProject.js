295d3e3bc6529b0c2ac85de860f61ba1f54aa7bd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-13 13:52:19 +0100

    chore:write the latest projects contents & links

--- Components/LatestProject.js
+++ Components/LatestProject.js
@@ -48,10 +48,11 @@ const LatestProject = () => {
                         </Link>
                         <p className="service-text">
                           Technologies used to developed Keyonaville schools are
-                          Javascript, Next.js, React.js, Chakra-Ui, etc.
+                          Javascript, Next.js, React.js, Chakra-Ui, HTML5, CSS3,
+                          SASS, React libraries, etc.
                         </p>
                         <Link
-                          href="https://www.google.com"
+                          href="https://www.keyonavilleschools.com.ng"
                           target="_blank"
                           rel="nofollow"
                         >
@@ -79,8 +80,10 @@ const LatestProject = () => {
                           <h3>E-Commerce Website</h3>
                         </Link>
                         <p className="service-text">
-                          Technologies used to developed bridgekode are
-                          Javascript, Next.js, React.js, Chakra-Ui, etc.
+                          Technologies used to developed These E-commerce
+                          website are Content Management System(CMS), Payment
+                          integration, plugins, CSS3, Google analytic, Livechat
+                          bot, backup wizzard, etc.
                         </p>
                         <Link
                           href="https://www.google.com"
@@ -112,10 +115,11 @@ const LatestProject = () => {
                         </Link>
                         <p className="service-text">
                           Technologies used to developed Keyonaville school
-                          portal are PHP, MySQL, laravel, Mantie, etc.
+                          portal & database are PHP, MySQL, laravel, Mantie,
+                          other libraries etc.
                         </p>
                         <Link
-                          href="https://www.google.com"
+                          href="https://admin.keyonavilleschools.com.ng"
                           target="_blank"
                           rel="nofollow"
                         >
@@ -140,14 +144,15 @@ const LatestProject = () => {
                       />
                       <Box className="project__contents">
                         <Link href="/projects/blog">
-                          <h3>Bridgetech Blogging</h3>
+                          <h3>Bridgekode Blogging</h3>
                         </Link>
                         <p className="service-text">
                           The technologies used for these project are Contents
-                          Management system[CMS], plugins, CSS etc
+                          Management system[CMS], plugins, CSS3, Google
+                          analytic, Livechat bot, backup wizzard, etc
                         </p>
                         <Link
-                          href="https://www.google.com"
+                          href="https://www.bridgekodeblog.com"
                           target="_blank"
                           rel="nofollow"
                         >
@@ -172,16 +177,17 @@ const LatestProject = () => {
                       />
                       <Box className="project__contents">
                         <Link href=" /projects/training">
-                          <h3>Web Dev Training</h3>
+                          <h3>ICT Website</h3>
                         </Link>
                         <p className="service-text">
-                          Bridgekode train students/kids on web development with
-                          HTML, CSS, Bootstrap, WordPress and JavaScript
+                          Technologies used to developed Florintech Computer
+                          College website are Javascript, Next.js, React.js,
+                          Chakra-Ui, CSS3, HTML5, livechat plugin, other
+                          libraries etc.
                         </p>
                         <Link
-                          href="https://www.google.com"
+                          href="https://www.florintechcomputercollege.com"
                           target="_blank"
-                          rel="nofollow"
                         >
                           <Image
                             src={arrow}
