0034bbeb320f43a6727ecb578c082b713dabbde5
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-14 01:08:23 +0100

    chore:added one object to the course data/json

--- Components/Courses/CourseData.js
+++ Components/Courses/CourseData.js
@@ -1,13 +1,14 @@
 const CourseDetails = [
-{
+  {
     id: "1",
-    courseImage:"",
-    title:"",
-    price:"",
-    contents:"",
-    courseUrl:"",
-},
-
-]
+    courseImage: "/images/web.jpg",
+    title: "Web Development",
+    price: "# 100,000",
+    courseUrl: "/courses/web-development",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ut suscipit velit voluptas iusto neque, magni porro incidunt? Assumenda magnam unde distinctio libero fugiat accusantium qui officia natus, nemo consequuntur.",
+  },
+];
 
 export default CourseDetails;
+<p></p>;
