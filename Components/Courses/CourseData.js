d68ad7e5ddf7718d42754082a6fb82eea80a31fb
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-16 23:52:28 +0100

    chore:done with all the courses summary

--- Components/Courses/CourseData.js
+++ Components/Courses/CourseData.js
@@ -28,13 +28,14 @@ const CourseDetails = [
   },
   {
     id: "4",
-    courseImage: "/images/project7.jpg",
-    title: "Branding & SEO",
-    price: "#45,000",
-    courseUrl: "/courses/branding",
+    courseImage: "/images/web.jpg",
+    title: "students/Kids Coding",
+    price: "#50,000",
+    courseUrl: "/courses/kids-coding",
     contents:
-      "This course focuses on optimizing websites to improve their visibility to clients in search engine results.It  delves into the principles & strategies of branding, like creating brand identities, developing brand messaging, & gain the crucials of consistency & differentiation.",
+      "This course will introduces children, students, secondary leavers to coding concepts in a fun and interactive manner. It may cover basic web/programming concepts using visual tools & programming languages or block-based coding platforms.",
   },
+
   {
     id: "5",
     courseImage: "/images/project3.jpg",
@@ -47,12 +48,12 @@ const CourseDetails = [
 
   {
     id: "6",
-    courseImage: "/images/web.jpg",
-    title: "students/Kids Coding",
-    price: "#50,000",
-    courseUrl: "/courses/kids-coding",
+    courseImage: "/images/project7.jpg",
+    title: "Branding & SEO",
+    price: "#45,000",
+    courseUrl: "/courses/branding",
     contents:
-      "This course will introduces children, students, secondary leavers to coding concepts in a fun and interactive manner. It may cover basic web/programming concepts using visual tools & programming languages or block-based coding platforms.",
+      "This course focuses on optimizing websites to improve their visibility to clients in search engine results.It  delves into the principles & strategies of branding, like creating brand identities, developing brand messaging, & gain the crucials of consistency & differentiation.",
   },
   {
     id: "7",
@@ -64,7 +65,7 @@ const CourseDetails = [
       "This course will provides  a valuable skill in the digital age, as high-quality content is crucial for effective communication & engagement with online audiences. With time & dedication, you can become a skilled content writer ",
   },
   {
-    id: "7",
+    id: "8",
     courseImage: "/images/project3.jpg",
     title: "Office Applications",
     price: "#45,000",
@@ -73,13 +74,13 @@ const CourseDetails = [
       "This course focuses on the Microsoft Office Suite, including Word, Excel, PowerPoint, and Outlook. It will cover essential skills like document formatting, data analysis, presentation creation, and email management.",
   },
   {
-    id: "7",
+    id: "9",
     courseImage: "/images/project7.jpg",
     title: "Desktop Publishing",
-    price: "#45,000",
+    price: "#50,000",
     courseUrl: "/courses/contents-writing",
     contents:
-      "This course focuses on the Microsoft Office Suite, including Word, Excel, PowerPoint, and Outlook. It will cover essential skills like document formatting, data analysis, presentation creation, and email management.",
+      "This involve creating and arranging digital content for print or electronic distribution. It involves designing and formatting various types of publications, such as brochures, flyers, magazines, newsletters, books, and other printed materials.",
   },
 ];
 
