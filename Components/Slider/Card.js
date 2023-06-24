b91beeb1bac6bf52e53f9c297ed0666556bd18e3
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-06-24 23:51:34 +0100

    chore:slider btn links

--- Components/Slider/Card.js
+++ Components/Slider/Card.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import Link from "next-link";
+import Image from "next-image";
 import Image__slider1 from "../../public/images/software.jpg";
 import Image__slider2 from "../../public/images/graphic.jpg";
 import Image__slider3 from "../../public/images/flask.jpg";
@@ -11,7 +14,7 @@ const Card = ({ renderButtons, img, desc }) => {
       <Slider>
         <Card
           img={"https://picsum.photos/350/300"}
-          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+          desc="The power of technology unleashed for you and others like you."
           renderButtons={() => (
             <>
               <button>clissdas</button>
@@ -21,7 +24,7 @@ const Card = ({ renderButtons, img, desc }) => {
         />
         <Card
           img={"https://picsum.photos/300/300"}
-          desc="Lorem ipsum dolor sit amet consect"
+          desc="We try for the best and we deliver the best."
           renderButtons={() => (
             <>
               <button>REST</button>
@@ -31,7 +34,7 @@ const Card = ({ renderButtons, img, desc }) => {
         />
         <Card
           img={"https://picsum.photos/400/400"}
-          desc="Lorem ipsum dolor sit amet consect sdfdsf .jhksd,jas dasd as daghvsd vasd as da"
+          desc="We inspire from technology to inspire the technology."
           renderButtons={() => (
             <>
               <button>Own the day</button>
@@ -56,3 +59,29 @@ const Card = ({ renderButtons, img, desc }) => {
 };
 
 export default Card;
+
+// For every complex problem, we give you a simple solution.
+// A good programmer looks both ways before crossing a one-way street.
+// Techs for people to the people.
+// Don’t just have a website, have a better website.
+// We have all the web tools you need to get ahead of your competition.
+
+// 
+// team
+// about
+
+// 
+// facebook
+// instagram
+
+// 
+// courses
+// faq
+
+// 
+// contact
+// make call
+
+// 
+// blog
+// whatsapp us
