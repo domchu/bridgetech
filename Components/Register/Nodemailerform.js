ebf8f95c2d26418c855060b3d346eae2e1882d1a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-02 12:17:18 +0100

    chore:create chakra form & imported to reg comp

---
+++ Components/Register/Nodemailerform.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nodemailerform = () => {
+  return (
+    <>
+      <h1>Nodemailerform </h1>
+    </>
+  );
+};
+
+export default Nodemailerform;
--- pages/access/form.js
+++ pages/access/form.js
@@ -3,6 +3,7 @@ import Header from "../../Components/Header/Header";
 import Topnav from "../../Components/Topnav/Topnav";
 import Footer from "../../Components/Footer/Footer";
 import Register from "../../Components/Register/Register";
+import Nodemailerform from "../../Components/Register/Nodemailerform";
 import NewsLetter from "../../Components/NewsLetter";
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo.config";
@@ -18,6 +19,7 @@ const form = () => {
       <Topnav />
       <Header />
       <Register />
+      <Nodemailerform />
       <NewsLetter />
       <Footer />
     </>
