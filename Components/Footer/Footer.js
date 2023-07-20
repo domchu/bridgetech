68dc9336cbeae9990cb3f6927a997074f02c5e73
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-20 22:05:43 +0100

    chore:change the head office address

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -205,7 +205,7 @@ const Footer = () => {
               <Box>
                 <p>
                   <strong style={{ color: "#fff" }}>Head Office :</strong> Shop
-                  234, N0.53 Igbede Road, Alaba Int'l Market, Ojo-Lagos State.
+                  40, Abuja Plaza Ojo Alaba Int'l Market, Ojo-Lagos State.
                   Nigeria
                 </p>
               </Box>
