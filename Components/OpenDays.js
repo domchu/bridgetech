b9b0c80e16ed00013e07dbfeda5b8f1f201a3456
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-29 18:13:17 +0100

    chore:styles & link to call when office is closed

--- Components/OpenDays.js
+++ Components/OpenDays.js
@@ -1,73 +1,136 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
-
+import Link from "next/link";
 
 const OpenDays = () => {
+  const [getDays, setGetDays] = useState();
 
-    const [getDays, setGetDays] = useState()
+  const activateDays = () => {
+    let today = new Date();
+    let currentDay = today.getDay();
+    setGetDays(currentDay);
+    // console.log(currentDay)
+    console.log(today);
+  };
 
-    const activateDays = () => {
-        let today = new Date();
-        let currentDay = today.getDay()
-        setGetDays(currentDay)
-        // console.log(currentDay)
-        console.log(today)
-    }
-
-    useEffect(() => {
-        activateDays
-    }, [])
-    return (
-        <>
+  useEffect(() => {
+    activateDays;
+  }, []);
+  return (
+    <>
+      <Box>
         <Box>
-                <Box>
-                    
-                    <Box width="25%" textAlign="left">
-                        <Box display="flex" justifyContent="space-around" fontWeight="bold" fontSize="1.5rem" color="#023e8a"> 
-                            <span>Open Days</span>
-                            <span>Open Hours</span>
-                        </Box>
-                        <br />
-                        <Box className={getDays == 1 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around", textAlign:"left", alignItems:"left"}}>
-                            <span>Mondays</span>
-                            <span>9:00AM - 5:00PM</span> 
-                        </Box>
-                        <br/>
-                        <li className={getDays == 2 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around" , textAlign:"left", }}>
-                            <span>Tuesdays</span>
-                            <span>9:00AM - 5:00PM</span>
-                        </li>
-                        <br/>
-                        <li className={getDays == 3 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around" , textAlign:"left", }}>
-                            <span>Wednesdays</span>
-                            <span>9:00AM - 5:00PM</span>
-                        </li>
-                        <br/>
-                        <li className={getDays == 4 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around"}}>
-                            <span>Thursdays</span>
-                            <span>9:00AM - 5:00PM</span>
-                        </li>
-                        <br/>
-                        <li className={getDays == 5 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around"}}>
-                            <span>Fridays</span>
-                            <span>9:00AM - 5:00PM</span>
-                        </li>
-                        <br/>
-                        <li className={getDays == 6 ? "open" : "days"} style={{display:"flex", justifyContent:"space-around"}}>
-                            <span>Saturdays</span>
-                            <span>Office Closed</span>
-                        </li>
-                        <br/>
-                        <li className={getDays == 0 ? "active" : "days"} style={{display:"flex", justifyContent:"space-around"}}>
-                            <span>Sundays</span>
-                            <span>Office Closed</span>
-                        </li>
-                      
-                    </Box>
-</Box>
-                
+          <Box width="25%" textAlign="left" mb="2rem">
+            <Box
+              display="flex"
+              justifyContent="space-around"
+              fontWeight="bold"
+              fontSize="1.5rem"
+              color="#023e8a"
+              textAlign="left"
+            >
+              <span>Open Days</span>
+              <span>Open Hours</span>
+            </Box>
+            <br />
+            <Box
+              className={getDays == 1 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                textAlign: "left",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Mondays</span>
+              <span>9:00 AM - 5:00 PM</span>
+            </Box>
+            <br />
+            <li
+              className={getDays == 2 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Tuesdays</span>
+              <span>9:00 AM - 5:00 PM</span>
+            </li>
+            <br />
+            <li
+              className={getDays == 3 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Wednesdays</span>
+              <span>9:00 AM - 5:00 PM</span>
+            </li>
+            <br />
+            <li
+              className={getDays == 4 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Thursdays</span>
+              <span>9:00 AM - 5:00 PM</span>
+            </li>
+            <br />
+            <li
+              className={getDays == 5 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Fridays</span>
+              <span>9:00 AM - 5:00 PM</span>
+            </li>
+            <br />
+            <li
+              //   className={getDays == 6 ? "open" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Saturdays</span>
+              <span>
+                Closed
+                <Link href="tel:+234 7034278995" className="primary-button">
+                  Call Us
+                </Link>
+              </span>
+            </li>
+            <br />
+            <li
+              //   className={getDays == 0 ? "active" : "days"}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                fontSize: "1.2rem",
+              }}
+            >
+              <span>Sundays</span>
+              <span>
+                Closed
+                <a href="tel:+234 7034278995" className="primary-button">
+                  Call Us
+                </a>
+              </span>
+            </li>
+          </Box>
         </Box>
-        </>
-    )
-}
+      </Box>
+    </>
+  );
+};
 export default OpenDays;
