575c2fdce38772e80a368dd9f52d917ebb8fdbfa
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-08-09 14:54:14 +0100

    chore:exclude the project page in the app

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,11 @@
 module.exports = {
   siteUrl: "https://www.bridgekode.com",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", disallow: "/project" },
+      { userAgent: "*", allow: "/" },
+    ],
+  },
+  exclude: ["/project"],
 };
